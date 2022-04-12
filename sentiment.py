1e3c02d7ca3c67918458e92dd00a8fe4d36278a6
Sriram S <srirammarirs22@gmail.com>
2022-04-12 22:21:09 +0530

    Create sentiment.py

---
+++ sentiment.py
@@ -0,0 +1,21 @@
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+nltk.download('wordnet')
+from nltk.corpus import wordnet
+
+# POS tagger dictionary
+pos_dict = {'J':wordnet.ADJ, 'V':wordnet.VERB, 'N':wordnet.NOUN, 'R':wordnet.ADV}
+def token_stop_pos(text):
+    tags = pos_tag(word_tokenize(text))
+    newlist = []
+    for word, tag in tags:
+        if word.lower() not in set(stopwords.words('english')):
+        newlist.append(tuple([word, pos_dict.get(tag[0])]))
+    return newlist
+
+mydata['POS tagged'] = mydata['Cleaned Reviews'].apply(token_stop_pos)
+mydata.head()
